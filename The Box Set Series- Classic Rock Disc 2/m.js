bfd505ecfd1f015d990ff446be29860d26890382
littleflute <13699175164@sina.cn>
2017-06-30 14:52:55 -0500

    Add files via upload

---
+++ The Box Set Series- Classic Rock Disc 2/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Rock and Roll, Hoochie Koo.mp3";		
+f[2]	= "02 Still Alive and Well.mp3";		
+f[3]	= "03 Freeway Jam.mp3";		
+f[4]	= "05 Black Magic Woman-Gypsy Queen.mp3";		
+f[5]	= "06 Albert's Shuffle.mp3";		
+f[6]	= "07 Pride - Joy.mp3";		
+f[7]	= "08 Always with Me, Always with You.mp3";		
+f[8]	= "09 Cult of Personality.mp3";		
+f[9]	= "10 For the Love of God.mp3";		
+f[10]	= "Stranglehold.mp3";		
