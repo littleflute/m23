60b880c3fba0bf5133d980e53945813388fd0de9
littleflute <13699175164@sina.cn>
2017-06-30 14:58:46 -0500

    Add files via upload

---
+++ The Smiths/The Smiths/m.js
@@ -0,0 +1,13 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Reel Around the Fountain.mp3";		
+f[2]	= "02 You've Got Everything Now.mp3";		
+f[3]	= "03 Miserable Lie.mp3";		
+f[4]	= "04 Pretty Girls Make Graves.mp3";		
+f[5]	= "05 The Hand That Rocks the Cradle.mp3";		
+f[6]	= "06 This Charming Man.mp3";		
+f[7]	= "07 Still Ill.mp3";		
+f[8]	= "08 Hand in Glove.mp3";		
+f[9]	= "09 What Difference Does It Make-.mp3";		
+f[10]	= "10 I Don't Owe You Anything.mp3";		
+f[11]	= "11 Suffer Little Children.mp3";		
